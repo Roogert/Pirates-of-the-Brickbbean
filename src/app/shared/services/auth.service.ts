813d9643060de5fc49dfac462d665830501c996d
MichaelAI0 <99698667+MichaelAl0@users.noreply.github.com>
2023-04-17 20:18:47 -0500

    added auth functions

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -4,30 +4,32 @@ import { BehaviorSubject } from 'rxjs';
 import { Router } from '@angular/router';
 
 interface LoginResponse {
-  jwt:string;
+  jwt: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   private user = new BehaviorSubject<any>(null);
 
-  getCurrentUsers(){
+  getCurrentUsers() {
     return !!this.user.value;
   }
 
-  login(email:string, password:string){
+  login(email: string, password: string) {
     return this.http
-    .post<LoginResponse>('http://localhost:3000/api/v1/users/login',{
-      email, password, password_confirmation: password,
-    })
-    .subscribe((response: any) => {
-      this.user.next(response.payload.user);
-    })
+      .post<LoginResponse>('http://localhost:3000/api/v1/users/login', {
+        email,
+        password,
+        password_confirmation: password,
+      })
+      .subscribe((response: any) => {
+        this.user.next(response.payload.user);
+        this.router.navigate(['']);
+      });
   }
 
   logout() {
@@ -38,4 +40,5 @@ export class AuthService {
       });
   }
 
+  isLoggedIn() {}
 }
